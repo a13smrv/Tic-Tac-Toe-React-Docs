78b021407625500de943ab78183676f6b51cc7b0
Alexander Smurov <a13smrv@gmail.com>
2022-11-06 15:37:00 +0100

    feat: add cell click event handling

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => { console.log(this); }}>
         {this.props.value}
       </button>
     );
