86d12c2e0eb920f836386950fe33f376499b0b27
Alexander Smurov <a13smrv@gmail.com>
2022-11-07 11:06:14 +0100

    feat: update logic in Square component
    - Delete "state" from the Square component.
    - Display necessary information from "props".
    - Add argument for the passing function to Square through "props" inside renderSquare().

--- src/index.js
+++ src/index.js
@@ -3,20 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
-
   render() {
     return (
       <button
         className="square"
-        onClick={() => this.setState({ value: "X" })}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -40,7 +33,7 @@ class Board extends React.Component {
     return (
       <Square
         value={this.state.squares[i]}
-        onClick={() => this.handleClick}
+        onClick={() => this.handleClick(i)}
       />
     );
   }
